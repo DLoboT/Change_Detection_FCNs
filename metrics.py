d593be642a27dba37ef92cddf1e2b7a110e34c61
DLoboT <72670364+DLoboT@users.noreply.github.com>
2021-07-17 12:49:51 -0300

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -161,27 +161,6 @@ if __name__=='__main__':
     # Split Image in Tails
     tiles_Image = split_tail(rows, cols, no_tiles_h, no_tiles_w)
     if args. Mask_P_M: 
-        # Train_tiles = np.array([2, 6, 13, 24, 28, 35, 37, 46, 47, 53, 58, 60, 64, 71, 75, 82, 86, 88, 93])
-        # Valid_tiles = np.array([8, 11, 26, 49, 78])
-
-        # Train_tiles = np.array([6, 13, 23, 24, 25, 35,47, 48, 49, 50, 60, 70, 71, 75, 95, 96, 97, 98])
-        # Valid_tiles = np.array([8, 36, 37, 63, 85])
-      
-        # Train_tiles = np.array([21, 23, 24, 32, 34, 37, 44, 57, 65, 68, 87, 89, 90, 9, 72, 74, 16, 82, 73, 27, 39,  7, 15, 51, 86]) 
-        # # 19, 28, 96, 94, 97, 17, 18, 31, 52,  5, 30, 81])
-        # Valid_tiles = np.array([40, 43, 67, 76, 100, 4, 3, 83, 92, 36])
-        
-        # Train_tiles = np.array([25, 26, 27, 39, 49, 50, 56, 59, 60, 65, 67, 76, 78, 53, 21, 88, 10, 11, 15, 20, 18, 72, 86,  8,  4, 64]) 
-        # Valid_tiles = np.array([12, 22, 74, 75, 89, 90, 91, 100, 19, 17, 73, 63,  9, 31, 82, 98])
-
-        # Train_tiles = np.array([25, 26, 27, 39, 49, 50, 56, 59, 60, 65, 67, 76, 78]) 
-        # Valid_tiles = np.array([12, 22, 74, 75, 89, 90, 91, 100])
-        # Test_tiles = np.array([1,  2,  3, 13, 14, 23, 24, 30, 33, 34, 35, 36, 37, 38, 40, 44, 45, 46, 47, 48, 55, 57, 58, 66, 68, 69, 70, 77, 79, 80])
-
-        # Test_tiles = np.arange(100)  
-        # Test_tiles = list(set(Test_tiles) - set(Train_tiles))
-        # Test_tiles = list(set(Test_tiles) - set(Valid_tiles))
-
         Train_tiles = np.array([13, 14, 15]) 
         Valid_tiles = np.array([9])
         Test_tiles = np.array([1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12])
@@ -262,22 +241,12 @@ if __name__=='__main__':
         # test_mask[mask == 1] = 1    # This is to chose the train region of the mask
         
         ######## Not taking into account the past desforestation
-        test_mask[reference == 2] = 0   #############  UNCOMMENT ############
+        test_mask[reference == 2] = 0   
                         
-        less_6ha_predict = predict_labels - area_opening(predict_labels.astype('int'), #############  UNCOMMENT ############
-                                            area_threshold = _6ha_area, connectivity=1) #############  UNCOMMENT ############
-        test_mask[less_6ha_predict == 1] = 0  #############  UNCOMMENT ############
+        less_6ha_predict = predict_labels - area_opening(predict_labels.astype('int'), 
+                                            area_threshold = _6ha_area, connectivity=1)
+        test_mask[less_6ha_predict == 1] = 0  
 
-        ################Loss Implementation ################
-        # y = Hot_encoding(reference)  
-        # # q image with 3d of the weights
-        # q = np.zeros_like(y)
-        # q[reference==0] = args.weights[0]
-        # q[reference==1] = args.weights[1]   
-        # y_loss = loss(y, predict_probs, q)
-        # print("Run number %d" %(k))
-        # print(y_loss[test_mask == 1].mean(axis=0))
-        # del y_loss
         ################ If I want to remove the area of 69 pixeles in the reference ####################################           
         if args.remove_69_ref:      
             less_6ha_ref = act_reference - area_opening(act_reference.astype('int'),
@@ -301,36 +270,6 @@ if __name__=='__main__':
 
         # Multiprocessing thresholds.
         Pmax = np.max(predict_probs[:,:,1][test_mask == 1])
-        
-        ################ Threshold Landsat ############
-        # Segnet
-        # th1, the2, th3 = 1.2e-2, 8.5e-3, 0.0
-        # u1, u2, u3 = 200, 600, 200
-        
-        # Unet
-        # th1, th2, th3 = 0.998, 0.1,  0.0
-        # u1, u2, u3 = 400, 100, 500
-
-        # Xception
-        # th1, th2, th3 = 0.02, 0.001,  0.0
-        # u1, u2, u3 = 200, 600, 200
-
-        # Mobile
-        # th1, th2, th3 = 0.998, 0.0014,  0.0
-        # u1, u2, u3 = 300, 100, 600
-
-        # DenseNet
-        # th1, th2, th3 = 0.998, 0.002,  0.0
-        # u1, u2, u3 = 300, 100, 600
-
-        # ResUNet
-        # th1, th2, th3 = 0.99, 0.005,  0.0
-        # u1, u2, u3 = 100, 200,  1700
-        # Thresholds = np.concatenate((np.linspace(Pmax, th1, u1), 
-        #                              np.linspace(th1 - 1e-6, th2, u2),
-        #                              np.linspace(th2 - 1e-6, th3, u3)))
-
-        ################ Threshold Sentinel2 ############
         th1, th2 = 0.02, 0.0
         u1, u2 = 200, 1800
         Thresholds = np.concatenate((np.linspace(Pmax, th1, u1),
@@ -415,164 +354,4 @@ if __name__=='__main__':
     test_mask_see = gray2rgb(np.uint(test_mask))
     cv2.imwrite('./test_mask.tiff', test_mask_see)
 
-    # ############## Mean Prediction metrics ###################
-    # predict_labels = mean_probs.argmax(axis=-1)
-    # predict_labels[predict_labels == 2] = 0
-            
-    # # Test mask
-    # test_mask = np.zeros_like(mask)
-    # test_mask[mask == 0] = 1    # This is to choose the test region of the mask
-    # ######## Not taking into account the past desforestation
-    # test_mask[reference == 2] = 0
-                    
-    # less_6ha_predict = predict_labels - area_opening(predict_labels.astype('int'),
-    #                                     area_threshold = _6ha_area, connectivity=1)
-    # test_mask[less_6ha_predict == 1] = 0
-
-    # ################ If I want to remove the area of 69 pixeles in the reference ####################################           
-    # if args.remove_69_ref:      
-    #     less_6ha_ref = act_reference - area_opening(act_reference.astype('int'),
-    #                                         area_threshold = _6ha_area, connectivity=1)
-    #     test_mask[less_6ha_ref == 1] = 0
-    #     print(np.unique(less_6ha_ref))
-    
-    # y_true = act_reference[test_mask == 1]   
-    # y_pred = predict_labels[test_mask == 1]
-        
-    # accu, F1, Prec, R, Iou, Alarm_Area, tn, fp, fn, tp = metrics(y_true, y_pred)
-    # copy_txt("Mean_map",accu, F1, Prec, R, IoU, AA, tn, fp, fn, tp)
-        
-    # # Multiprocessing thresholds.
-    # Pmax = np.max(mean_probs[:,:,1][act_reference * test_mask == 1])
-    # Thresholds = np.linspace(Pmax, 0, args.Npoints)
-    
-    # mean_probs = mean_probs[test_mask == 1]
-    # del less_6ha_predict, Norm_image, mask # saving memory
-
-    # from utils import do
-    # metrics_list = do(Thresholds, mean_probs, y_true)
-
-    # metrics_list = np.asarray(metrics_list).transpose()
-    # accuracy  = metrics_list[0, :]
-    # f1_score  = metrics_list[1, :]
-    # precision = metrics_list[2, :]
-    # recall    = metrics_list[3, :]
-    # IoU       = metrics_list[4, :]
-    # AA        = metrics_list[5, :]
-
-    # np.save("Threshold_mean_map_%d_%d"%(Arq, k),Thresholds)
-    # np.save("Recall_mean_map_%d_%d"%(Arq, k),recall)
-    # np.save("Precision_mean_map_%d_%d"%(Arq, k),precision)
-    # np.save("Alarm Area_mean_map_%d_%d"%(Arq, k), AA)       
-    
-    
-    ###################################################
-        #plt.plot(np.array(recall)*100, np.array(AA)*100)
-        ## naming the x axis 
-        #plt.xlabel('Recall (%)') 
-        ## naming the y axis 
-        #plt.ylabel('Alarm Area (%)') 
-        #plt.xticks([80, 85, 90, 95, 100])
-        #plt.yticks([0,10,20,30,40,50,60,70,80,90,100])
-        #plt.grid(True)
-        ## giving a title to my graph 
-        #plt.title('Alarm Area vs Recall')   
-        #plt.show()
-        
-
-        # =============================================================================            
-        ######################## See the activation of intermediate layers #####################           
-        #        if see_intermediate_layers:
-        #            test_patches = []
-        #            patch_im_test = []
-        #            patch_gdt_test = []
-        #
-        #            step_row = (stride - rows % stride) % stride
-        #            step_col = (stride - cols % stride) % stride
-        #            k1, k2 = (rows + step_row)//stride, (cols + step_col)//stride
-        #            
-        #            for i in range(k1):
-        #                for j in range(k2):
-        #                    # Test
-        #                    if test_mask[i*stride:i*stride + args.patch_size, j*stride:j*stride + args.patch_size].all():
-        #                        test_patches.append((i*stride, j*stride)) 
-        #            
-        #            pad_tuple_img = ( (overlap//2, overlap//2 + step_row), (overlap//2, overlap//2 + step_col), (0, 0) )
-        #            pad_tuple_msk = ( (overlap//2, overlap//2 + step_row), (overlap//2, overlap//2 + step_col) )
-        #            img_pad = np.pad(Norm_image, pad_tuple_img, mode = 'symmetric')
-        #            gdt_pad = np.pad(reference, pad_tuple_msk, mode = 'symmetric')
-        #            
-        ##            Patch of Images
-        #            I_patch = img_pad[test_patches[48][0]: test_patches[48][0] + args.patch_size, test_patches[48][1]: test_patches[48][1] + patch_size,:]
-        #            patch_im_test.append(I_patch)  
-        #            
-        ##           Patch of References                    
-        #            gdt_patch = gdt_pad[test_patches[48][0]: test_patches[48][0] + args.patch_size, test_patches[48][1]: test_patches[48][1] + patch_size]
-        #            patch_gdt_test.append(gdt_patch) 
-        #            
-        #            patch_im_test = np.array(patch_im_test)
-        #            patch_gdt_test = np.array(patch_gdt_test)
-        ##            print(np.array(patch_im_test).shape)
-        #           
-        ##            plt.figure()
-        ##            imgplot = plt.imshow(patch_im_test[:,:,:,[3,2,1]][0])
-        ##            plt.show()   
-        ##
-        ##            plt.figure()
-        ##            imgplot = plt.imshow(patch_im_test[:,:,:,[3+8,2+8,1+8]][0])
-        ##            plt.show()   
-        ##            
-        ##            plt.figure()
-        ##            imgplot = plt.imshow(patch_gdt_test[0])
-        ##            plt.show()   
-        #
-        #            layer_name =  'conv_14' 
-        #            # intermediate layer, get output features
-        #            layer = Model(inputs=net.input, outputs=net.get_layer(layer_name).output)
-        #            
-        ##            This is the activation for thw fourth convolution layer for a patch of the deforestation image.           
-        #            layer_activations = layer.predict(patch_im_test)
-        #            layer_activations = layer_activations[0]           
-        ##            print(layer_activations.shape)
-        ##            plt.matshow(layer_activations[0,:,:,10], cmap ='viridis')
-        #            
-        ##            Visualizing every channel of a layer:
-        #            Visualize_activations(layer_name, layer_activations)
-        ## =============================================================================            
-        #            # Visualize weights for layer 
-        #            W = net.get_layer(name=layer_name).get_weights()[0]
-        #            W = np.squeeze(W)
-        #            W = W[:,:,0,:]
-        #
-        #            npad = ((1,1), (1,1), (0,0))
-        #            W = np.pad(W, pad_width = npad, mode='constant', constant_values=0)
-        #            
-        #            Visualize_activations(layer_name, W)
-        #          
-        ## =============================================================================          
-        ##            Using grad-cam technique:
-        #            heatmap = Visualization_heatmaps(layer_name, layer_activations, net, patch_im_test)
-        ## =============================================================================
-        #            #Resizes the heatmap to be the same size as the original image
-        #            up_heatmap = cv2.resize(heatmap,(patch_im_test.shape[1], patch_im_test.shape[2]))
-        #            
-        #            ##Convert it to RGB
-        #            up_heatmap =np.uint8(255 * up_heatmap)
-        #            
-        #            plt.imshow(patch_im_test)
-        #            plt.imshow(up_heatmap, alpha = 0.5)
-        #            plt.show()   
-        
-        # ============================================================================= 
-        # =============================================================================
-        
-        
-        # =============================================================================
-
-                        
-    #            print('Test accuracy:%.2f' %(100*accu))
-    #            print('Test f1score:%.2f' %(100*F1))
-    #            print('Test prescision:%.2f' %(100*Prec))
-    #            print('Test recall:%.2f' %(100*R))
-    #            print('Intersection over Union:%.2f' %(100*Iou))
-    #            print('Alarm Area:%.2f'%(100*Alarm_Area))
+   
